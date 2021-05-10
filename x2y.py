7da02457e91a85f8e2046b129094d2a8bd5ba3fd
html-3 <henriquemarques@ufrj.br>
2021-05-09 23:52:26 -0300

    1.5 Funcao universal

--- x2y.py
+++ x2y.py
@@ -1,2 +1,9 @@
-def(m,x,y):
+def(x,n,m):
+    # n é a base de x e m é a base a ser transformada
+    if n>m:
+        #
+    elif m>n:
+
+    else:
+        print("Esses valores estao na mesma base!")
     
