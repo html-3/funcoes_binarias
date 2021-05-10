7426de1fb1e48264f4141dbc8d278cdd168c085f
html-3 <henriquemarques@ufrj.br>
2021-05-10 00:00:51 -0300

    1.5 Funcao universal

--- x2y.py
+++ x2y.py
@@ -1,9 +1,24 @@
 def(x,n,m):
     # n é a base de x e m é a base a ser transformada
+    # n, m ∈ {1,2,3...20}
+    bases = {
+        10;"A",
+        11;"B",
+        12;"C",
+        13;"D",
+        14;"E",
+        15;"F",
+        16;"G",
+        17;"H",
+        18;"I",
+        19;"J",
+        20;"K",
+    }
     if n>m:
-        #
+        # x deve ser dividio por m
+    
     elif m>n:
-
+        # x deve ser multiplicado por m
     else:
-        print("Esses valores estao na mesma base!")
+        print("As bases sao as mesmas!")
     
